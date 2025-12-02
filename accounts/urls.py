a0d159398bd246b3be0fdbd93987bda68aa30383
Joemer <joemers97@gmail.com>
2025-12-02 15:14:02 -0500

    Implement full password reset functionality: - Add reset password templates and email template - Update accounts URLs and views for forgot/reset password - Improve login page link to password reset - Add .prettierignore to prevent formatting of Django templates - Update account verification email template

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("activate/<uidb64>/<token>/", views.activate, name="activate"),  # type: ignore
     path("forgotPassword/", views.forgotPassword, name="forgotPassword"),
+    path("resetpassword_validate/<uidb64>/<token>/", views.resetpassword_validate, name="resetpassword_validate"),  # type: ignore
+    path("resetPassword/", views.resetPassword, name="resetPassword"),  # type: ignore
 ]
