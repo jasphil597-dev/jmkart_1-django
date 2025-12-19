af4d0332c33e926e0047233aa21c2a5f0877e638
Joemer <joemers97@gmail.com>
2025-12-18 22:54:41 -0500

    feat(accounts): add user profile model, dashboard, and account management pages

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,17 +1,73 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Account
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import Account, UserProfile
+
 
 class AccountAdmin(UserAdmin):
-  list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
-  list_display_links = ('email', 'first_name', 'last_name') # this makes the email, first & last_name clickable
-  readonly_fields = ('last_login', 'date_joined')
-  ordering = ('-date_joined',) # this gives us descending list
-  
-  filter_horizontal = ()
-  list_filter = ()
-  fieldsets = ()  # this maakes password read only
+    list_display = (
+        'email',
+        'first_name',
+        'last_name',
+        'username',
+        'last_login',
+        'date_joined',
+        'is_active',
+    )
+    list_display_links = ('email', 'first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_joined')
+    ordering = ('-date_joined',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('thumbnail', 'user', 'city', 'state', 'country')
+
+    def thumbnail(self, obj):
+        if obj.profile_picture:
+            return format_html(
+                '<img src="{}" width="30" height="30" style="border-radius:50%;">',
+                obj.profile_picture.url,
+            )
+        return "-"
+
+    thumbnail.short_description = 'Profile Picture'
+
 
 admin.site.register(Account, AccountAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
+
+# ==============================================
+
+# My code:
+
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+# from .models import Account, UserProfile
+# from django.utils.html import format_html
+
+# # Register your models here.
+
+# class AccountAdmin(UserAdmin):
+#   list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
+#   list_display_links = ('email', 'first_name', 'last_name') # this makes the email, first & last_name clickable
+#   readonly_fields = ('last_login', 'date_joined')
+#   ordering = ('-date_joined',) # this gives us descending list
+
+#   filter_horizontal = ()
+#   list_filter = ()
+#   fieldsets = ()  # this maakes password read only
+
+# class UserProfileAdmin(admin.ModelAdmin):
+#     def thumbnail(self, object):
+#         return format_html('<img src="{}" width="30" style="border-radius:50%;">'.format(object.profile_picture.url))
+#     thumbnail.short_description = 'Profile Picture'
+#     list_display = ('thumbnail', 'user', 'city', 'state', 'country')
+
+# admin.site.register(Account, AccountAdmin)
+# admin.site.register(UserProfile, UserProfileAdmin)
+
--- accounts/urls.py
+++ accounts/urls.py
@@ -12,4 +12,9 @@ urlpatterns = [
     path("forgotPassword/", views.forgotPassword, name="forgotPassword"),
     path("resetpassword_validate/<uidb64>/<token>/", views.resetpassword_validate, name="resetpassword_validate"),  # type: ignore
     path("resetPassword/", views.resetPassword, name="resetPassword"),  # type: ignore
+
+    path("my_orders/", views.my_orders, name="my_orders"),
+    path("edit_profile/", views.edit_profile, name="edit_profile"),
+    path("change_password/", views.change_password, name="change_password"), # type: ignore
+    path("order_detail/<int:order_id>/", views.order_detail, name="order_detail"),# type: ignore
 ]
